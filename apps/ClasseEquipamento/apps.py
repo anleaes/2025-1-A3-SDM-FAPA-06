5d59e3ff8229d2f7bed7b1f5658898a680098b29
IFTnomai <iagoftubiana@gmail.com>
2025-06-01 18:26:24 -0300

    criação da classe ClassEquipament e dos Models

---
+++ apps/ClasseEquipamento/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ClassEquipmentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ClassEquipment'
+    verbose_name = 'ClasseEquipamento'
