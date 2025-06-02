2f7c5baf6f568dcfff3fdbe15f7e3eb8d4e9d6c5
IFTnomai <iagoftubiana@gmail.com>
2025-06-02 12:23:20 -0300

    implementação do serializer

--- apps/ClasseEquipamento/serializer.py
+++ apps/ClasseEquipamento/serializer.py
@@ -1,5 +1,10 @@
 from .models import ClasseEquipamento
 from rest_framework import serializers
 
+class ClassEquipamentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClasseEquipamento
+        fields = '__all__'
+        
 
         
