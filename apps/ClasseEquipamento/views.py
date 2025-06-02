b0a5fa88d34a4f2a771bf2f75a40feb60a61941e
IFTnomai <iagoftubiana@gmail.com>
2025-06-02 15:52:00 -0300

    criação do views do ClassEquipament e seus importes

--- apps/ClasseEquipamento/views.py
+++ apps/ClasseEquipamento/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import ClasseEquipamento 
+from rest_framework import viewsets
+from .serializer import ClassEquipamentSerializer
 
 # Create your views here.
+
