9d826bb7cc41aee2fc9d2eae4714e2df4b17cf64
IFTnomai <iagoftubiana@gmail.com>
2025-06-04 20:01:28 -0300

    Implementação do Serializer

--- apps/EquipamentoItem/serializer.py
+++ apps/EquipamentoItem/serializer.py
@@ -1,3 +1,9 @@
 from .models import EquipamentoItem 
 from rest_framework import serializers
 
+class EquipamentoItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EquipamentoItem
+        fields = '__all__'
+
+        
