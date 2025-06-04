1960b72dfe163b43ee71496eedb4269051e0c729
IFTnomai <iagoftubiana@gmail.com>
2025-06-04 15:50:42 -0300

    Implementação do Models Equipamento

--- apps/Equipamento/models.py
+++ apps/Equipamento/models.py
@@ -2,3 +2,21 @@ from django.db import models
 from ClasseEquipamento.models import ClasseEquipamento
 
 # Create your models here.
+
+class Equipamento(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
+    Modelo = models.CharField('Modelo', max_length=50)
+    Price = models.DecimalField('Preco', max_digits=10, decimal_places=2)
+    ClasseEquipamento = models.ForeignKey(ClasseEquipamento, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Equipamento'
+        verbose_name_plural = 'Equipamentos'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
+    
+
+    
