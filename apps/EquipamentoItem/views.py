607265fc7ab180cf2940753d136923ce59808906
IFTnomai <iagoftubiana@gmail.com>
2025-06-04 20:19:15 -0300

    Criação do Views e seus imports

--- apps/EquipamentoItem/views.py
+++ apps/EquipamentoItem/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import EquipamentoItem 
+from rest_framework import viewsets
+from .serializer import  EquipamentoItemSerializer
 
 # Create your views here.
+
