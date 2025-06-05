414c659755de2aa0714079a4e787d32ec12c926d
IFTnomai <iagoftubiana@gmail.com>
2025-06-05 16:53:06 -0300

    implementação do Urls

--- apps/Cliente/urls.py
+++ apps/Cliente/urls.py
@@ -2,3 +2,11 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+app_name = 'Cliente'
+
+router = routers.DefaultRouter()
+router.register('', views.ClienteViewSet, basename='Cliente')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
