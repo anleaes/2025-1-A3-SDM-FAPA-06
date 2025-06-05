58cf82273117840214162b74d725039fe85405a7
IFTnomai <iagoftubiana@gmail.com>
2025-06-04 22:53:20 -0300

    urls

--- apps/EquipamentoItem/urls.py
+++ apps/EquipamentoItem/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-app_name = 'orderitems'
+app_name = 'EquipamentoItem'
 
 router = routers.DefaultRouter()
-router.register('', views.OrderitemViewSet, basename='itens_pedido')
+router.register('', views.EquipamentoItemViewSet, basename='EquipamentoItem')
 
 urlpatterns = [
     path('', include(router.urls) )
