72efea304204277b5c1729979c68b0424aafacbe
IFTnomai <iagoftubiana@gmail.com>
2025-06-05 16:40:06 -0300

    Implementaçâo do Views

--- apps/Cliente/views.py
+++ apps/Cliente/views.py
@@ -3,3 +3,8 @@ from .models import Cliente
 from rest_framework import viewsets
 from .serializer import ClienteSerializer
 # Create your views here.
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer  
+    
