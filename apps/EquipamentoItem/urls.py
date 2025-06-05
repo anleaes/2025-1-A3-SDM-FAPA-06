6261384092dd1a9222ab5cdc67e858988a0e817d
IFTnomai <iagoftubiana@gmail.com>
2025-06-04 22:40:12 -0300

    Implementação do urls

--- apps/EquipamentoItem/urls.py
+++ apps/EquipamentoItem/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
 app_name = 'EquipamentoItem'
 
 router = routers.DefaultRouter()
