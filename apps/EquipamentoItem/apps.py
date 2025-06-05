2f91d4c5919f55df2ce54d5497e9c14c5547e3ee
IFTnomai <iagoftubiana@gmail.com>
2025-06-04 23:08:38 -0300

    Correção de erros

--- apps/EquipamentoItem/apps.py
+++ apps/EquipamentoItem/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class EquipamentoitemConfig(AppConfig):
+class EquipamentoItemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'EquipamentoItem'
     verbose_name = 'Equipamentos de Pedidos'
--- apps/EquipamentoItem/models.py
+++ apps/EquipamentoItem/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from Equipamento.models import Equipamento
-#from orders.models import Order
+from ClasseEquipamento.models import ClasseEquipamento
 # Create your models here.
 
-class Orderitem(models.Model):
+class EquipamentoItem(models.Model):
     quantity = models.PositiveIntegerField('Quantidade',null=True, blank=True,default=0)
     unitary_price = models.DecimalField('Preco unitario', max_digits=10, decimal_places=2)
     Equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE)
     
     
     class Meta:
-        unique_together = ('order', 'product')
         verbose_name = 'Equipamento de Pedido'
         verbose_name_plural = 'Equipamentos de Pedidos'
         ordering =['id']
