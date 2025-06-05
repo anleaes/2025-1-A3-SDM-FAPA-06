9ef9fb32a2486e4048d9a8f8555719c6c02352f3
IFTnomai <iagoftubiana@gmail.com>
2025-06-04 22:31:46 -0300

    implementação do Urls

--- apps/EquipamentoItem/urls.py
+++ apps/EquipamentoItem/urls.py
@@ -2,3 +2,11 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+app_name = 'EquipamentoItem'
+
+router = routers.DefaultRouter()
+router.register('', views.EquipamentoItemViewSet, basename='EquipamentoItem')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
