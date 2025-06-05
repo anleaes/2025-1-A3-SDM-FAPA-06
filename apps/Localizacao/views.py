3f86b5d820da345c0d2a03d11519ebc1f4e62ae7
IFTnomai <iagoftubiana@gmail.com>
2025-06-05 20:12:14 -0300

    Criação do Views da classe Localização

--- apps/Localizacao/views.py
+++ apps/Localizacao/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Localizacao
+from rest_framework import viewsets
+from .serializer import LocalizacaoSerializer
 
 # Create your views here.
+
