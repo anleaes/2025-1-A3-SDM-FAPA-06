d0fe7b4a89f7b5fbeb14256ff9962299532464c4
IFTnomai <iagoftubiana@gmail.com>
2025-06-05 20:02:33 -0300

    Implementação do Serializer

--- apps/Localizacao/serializer.py
+++ apps/Localizacao/serializer.py
@@ -1,3 +1,8 @@
-from .models import Cliente
+from .models import Localizacao
 from rest_framework import serializers
 
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Localizacao
+        fields = '__all__'
+        
