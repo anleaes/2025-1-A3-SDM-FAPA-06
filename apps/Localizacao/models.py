1f5e66b9d177779b5c699906ffb8186dca51f4f9
IFTnomai <iagoftubiana@gmail.com>
2025-06-05 19:58:03 -0300

    Implementação do Models

--- apps/Localizacao/models.py
+++ apps/Localizacao/models.py
@@ -2,3 +2,23 @@ from django.db import models
 from Cliente.models import Cliente
 
 # Create your models here.
+
+class Client(models.Model):
+    
+    address = models.CharField('Endereco', max_length=200)  
+    LOCATION_CHOICES = (
+        ('Casa'),
+        ('Predio'),
+        ('Empresa'),
+        ('Outro'),
+    )
+    Location = models.CharField('Localização', max_length=1, choices=LOCATION_CHOICES)
+    Cliente = models.ManyToManyField(Cliente, verbose_name="Cliente")
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering =['id']
+
+    def __str__(self):
+        return self.first_name
