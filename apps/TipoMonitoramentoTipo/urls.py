69ef84315daf87f6a7c6927a639554bb17dab224
IFTnomai <iagoftubiana@gmail.com>
2025-06-06 20:48:04 -0300

    Implementação do Urls

--- apps/TipoMonitoramentoTipo/urls.py
+++ apps/TipoMonitoramentoTipo/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'TipoMonitoramentoTipo'
+
+router = routers.DefaultRouter()
+router.register('', views.TipoMonitoramentoTipoViewSet, basename='TipoMonitoramentoTipo')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
