ab91a4c0280b4393df056a9e1b070f4bd3990356
IFTnomai <iagoftubiana@gmail.com>
2025-06-06 16:05:53 -0300

    Criação do Views e imports da classe Monitoramento

--- apps/Monitoramento/views.py
+++ apps/Monitoramento/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Monitoramento
+from rest_framework import viewsets
+from .serializer import MonitoramentoSerializer
 
 # Create your views here.
