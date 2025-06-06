2c3248b7202ee9de1763cf1075c1b67ce3bedec2
IFTnomai <iagoftubiana@gmail.com>
2025-06-06 20:44:53 -0300

    Implementação do Views

--- apps/TipoMonitoramentoTipo/views.py
+++ apps/TipoMonitoramentoTipo/views.py
@@ -6,3 +6,6 @@ from .serializer import TipoMonitoramentoTipoSerializer
 
 # Create your views here.
 
+class TipoMonitoramentoTipoViewSet(viewsets.ModelViewSet):
+    queryset = TipoMonitoramentoTipo.objects.all()
+    serializer_class = TipoMonitoramentoTipoSerializer  
