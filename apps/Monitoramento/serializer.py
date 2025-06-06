3d279cfd7c1b661ea87c95078047efa64915726d
IFTnomai <iagoftubiana@gmail.com>
2025-06-06 16:01:17 -0300

    Implementação do serializer

--- apps/Monitoramento/serializer.py
+++ apps/Monitoramento/serializer.py
@@ -1,3 +1,8 @@
 from .models import Monitoramento
 from rest_framework import serializers
 
+class MonitoramentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Monitoramento
+        fields = '__all__'
+        
