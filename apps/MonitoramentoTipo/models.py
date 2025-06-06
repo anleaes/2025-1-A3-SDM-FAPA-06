055bebdf2fe218868493a40d5faa620886fb8902
IFTnomai <iagoftubiana@gmail.com>
2025-06-06 16:48:41 -0300

    Implementação do Models

--- apps/MonitoramentoTipo/models.py
+++ apps/MonitoramentoTipo/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class MonitoramentoTipo(models.Model):
+    quantity = models.PositiveIntegerField('Quantidade',null=True, blank=True,default=0)
+    description = models.TextField('Descricao', max_length=100)
+    security = models.TextField('Seguranca', max_length=100)
+    rate_security = models.TextField('Grau de Seguranca', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Monitoramento Tipo'
+        verbose_name_plural = 'Monitoramento Tipos'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
