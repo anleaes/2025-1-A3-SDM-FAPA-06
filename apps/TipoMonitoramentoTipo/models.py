9adec3bc45ab546537e5ffb93787395489cd3ab6
IFTnomai <iagoftubiana@gmail.com>
2025-06-06 20:34:50 -0300

    Implementação do Models

--- apps/TipoMonitoramentoTipo/models.py
+++ apps/TipoMonitoramentoTipo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from Monitoramento.models import Monitoramento
+from MonitoramentoTipo.models import MonitoramentoTipo
 
 # Create your models here.
+
+class Order(models.Model):
+    Monitoramento = models.ForeignKey(Monitoramento, on_delete=models.CASCADE)
+    MonitoramentoTipo = models.ForeignKey(MonitoramentoTipo, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'TipoMonitoramentoTipo'
+        verbose_name_plural = 'Tipos de MonitoramentoTipo'
+        ordering =['id']
+
+    def __str__(self):
+        return "%s" % (self.client)
