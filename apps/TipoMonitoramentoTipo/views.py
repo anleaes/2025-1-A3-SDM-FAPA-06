e73a28ce1500f1e3065e7c620bacd480cfc45e46
IFTnomai <iagoftubiana@gmail.com>
2025-06-06 20:43:44 -0300

    Criação do Views da classe TipoMonitoramentoTipo

--- apps/TipoMonitoramentoTipo/views.py
+++ apps/TipoMonitoramentoTipo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import TipoMonitoramentoTipo
+from rest_framework import viewsets
+from .serializer import TipoMonitoramentoTipoSerializer
+
 
 # Create your views here.
+
