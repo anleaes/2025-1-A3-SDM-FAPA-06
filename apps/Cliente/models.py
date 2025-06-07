662ddb7e896b25293647f9721e30390a2754b4af
IFTnomai <iagoftubiana@gmail.com>
2025-06-07 15:30:34 -0300

    Implementação do SEARCH

--- apps/Cliente/models.py
+++ apps/Cliente/models.py
@@ -8,7 +8,7 @@ class Cliente(models.Model):
     address = models.CharField('Endereco', max_length=200)   
     cell_phone = models.CharField('Telefone celular', max_length=20)
     email = models.EmailField('E-mail',null=False, blank=False)
-    senha_numero = models.CharField('Senha', max_length=20, null=False, blank=False)
+    passcode = models.CharField('Senha', max_length=20, null=False, blank=False)
 
     class Meta:
         verbose_name = 'Cliente'
